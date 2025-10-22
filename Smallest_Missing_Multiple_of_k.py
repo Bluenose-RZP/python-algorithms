d5ca2b43850d5e390d91dc2030ce9c6b80e38be8
Bluenose-RZP <bluenose313@gmail.com>
2025-10-22 14:13:42 -0500

    Create Smallest_Missing_Multiple_of_k.py

---
+++ Smallest_Missing_Multiple_of_k.py
@@ -0,0 +1,18 @@
+'''Given an integer array numbs and an integer k, return the smallest positive multiple of k that is missing from numbs.
+a multiple of k is any positive integer divisible by k.
+example 1:
+input nums = [8,2,3,4,6] k = 2
+Output: 10
+This one was relatively easy for me and just took the one iteration. Time and space complexity are both O(n).
+'''
+
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        checkList = {}
+        mNum = 1
+        for number in nums:
+            checkList[number] = True
+        while True:
+            if not checkList.get(k * mNum):
+                return (k * mNum)
+            mNum += 1
