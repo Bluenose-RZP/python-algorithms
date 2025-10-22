5ca776c3291886b65f85f36510d2a5d0b800367d
Bluenose-RZP <bluenose313@gmail.com>
2025-10-22 14:23:31 -0500

    Update Smallest_Missing_Multiple_of_k.py
    Made a slight optimization rework

--- Smallest_Missing_Multiple_of_k.py
+++ Smallest_Missing_Multiple_of_k.py
@@ -5,7 +5,7 @@ input nums = [8,2,3,4,6] k = 2
 Output: 10
 This one was relatively easy for me and just took the one iteration. Time and space complexity are both O(n).
 '''
-
+'''
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
         checkList = {}
@@ -14,5 +14,16 @@ class Solution:
             checkList[number] = True
         while True:
             if not checkList.get(k * mNum):
+                return (k * mNum)
+            mNum += 1
+
+'''
+# I realized that using a set would be better than an empty dictionary and remade it for practice.
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        numSet = set(nums)
+        mNum = 1
+        while True:
+            if (k * mNum) not in numSet:
                 return (k * mNum)
             mNum += 1
